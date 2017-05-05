519ad6fd71cff8f7ba6334dbb088255e270608b4
evblance <evblance@users.noreply.github.com>
2017-05-06 02:17:24 +1000

    Completes basic layout

---
+++ main.js
@@ -0,0 +1,18 @@
+(function($){
+  $(document).ready(function() {
+
+    var Player = function() {
+      this.tiles = [];
+    };
+
+    var Computer = function() {
+      this.tiles = [];
+    };
+
+    Computer.prototype.move = function() {
+
+    };
+
+
+  });
+})(jQuery);
